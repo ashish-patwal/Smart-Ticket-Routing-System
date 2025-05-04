5ac3f791e2c477f6274e421dcc0d28fced1010c8
ashish-patwal <ashishpatwal147@gmail.com>
2025-05-04 13:04:14 +0530

    starter boilerplate

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return JSONResponse(content={"message": "Hello, World!"})
+
+@app.get("/hello")
+async def read_hello():
+    return JSONResponse(content={"message": "Hello, World!"})
+
+@app.get("/hello/{name}")
+async def read_hello_name(name: str):
+    return JSONResponse(content={"message": f"Hello, {name}!"})
 
 
